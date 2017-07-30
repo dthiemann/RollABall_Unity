4a56c0cd1c0520885d8e437e3201c43206097d5b
dthiemann <dylanthiemann@gmail.com>
2017-07-29 17:05:52 -0700

    Ball now rolls: https://unity3d.com/learn/tutorials/projects/roll-ball-tutorial/moving-camera?playlist=17141

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float speed;
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	// Called before rendering a frame - game code
+	void Update() {
+
+	}
+
+	// Called before any physics calculations
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
 	}
 }
